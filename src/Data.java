61c8f5186d47af585e15d85fe75c7dae40ccf8cf
Federico Schianchi <federicoschianchi2004@gmail.com>
2024-03-26 11:55:55 +0100

    First class kinda lame

---
+++ src/Data.java
@@ -0,0 +1,60 @@
+package src;
+
+public class Data {
+
+    private  int giorno;
+    private  int mese;
+    private  int anno;   //final non può essere più modificato,
+                                // quindi devono essere inizializzati nel costruttore
+
+    private boolean valid(int giorno, int mese, int anno){
+        return giorno >= 0 && giorno <= 31 && mese >= 1 && mese <= 12 && anno >= 0;
+    }
+
+    private void print(){
+        System.out.println("{ " + this.giorno + ", " + this.mese + ", " +  this.anno + " }");
+    }
+    public Data(int giorno, int mese, int anno) {
+        if (!valid(giorno, mese, anno))
+            throw new RuntimeException("Data non valida");
+
+        this.giorno = giorno;
+        this.mese = mese;
+        this.anno = anno;
+    }
+
+
+
+    public void setGiorno(int giorno) {
+        this.giorno = giorno;
+    }
+
+    public void setMese(int mese) {
+        this.mese = mese;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public int getGiorno() {
+        return giorno;
+    }
+
+    public int getMese() {
+        return mese;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public static void main(String[] args) {
+        Data d1 = new Data(12,2,2000); //d1 è una reference
+        //Data d2 = d1; //shallow copy, assegnamento fra reference
+        d1.print();
+
+
+
+    }
+}
--- src/FirstSin.java
+++ src/FirstSin.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class FirstSin {
     public static void main(String[] args) {
-        System.out.println("Isnerire tre numeri interi: ");
+        System.out.println("Numero elementi: ");
         Scanner sc = new Scanner(System.in);
 
-        int i, j, k;
+        int n = sc.nextInt();
+        if (n <= 0)
+            throw new RuntimeException("Valore negativo, must be positivo"); //exeption are obj
 
-        i = sc.nextInt();
-        j = sc.nextInt();
-        k = sc.nextInt();
+      int s = 0;
+      for (int i = 0; i < n; i++){
+          System.out.println("Inserires un valore: ");
+          s += sc.nextInt();
 
-        float m = ((float)(i+j+k)/3); // could be also (i+j+k)/3.0f
+      }
 
-        System.out.println("La media dei 3 numeri: " + m);
+        float m = ((float)(s)/n); // could be also (i+j+k)/3.0f
+
+        System.out.println("La media dei " + n + " numeri: " + m);
         sc.close();
     }
 }
